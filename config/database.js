498f10cb8a2a3e3a9cf0c266a089865aafe9b583
Nishit <94426361+NishitP1@users.noreply.github.com>
2025-03-28 06:32:45 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
-
+//hardcode the value if env is not working
 const pool = mysql.createPool({
-    host: process.env.DB_HOST ,
-    user: process.env.DB_USER ,
-    password: process.env.DB_PASSWORD ,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'whatsapp_reminder',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
