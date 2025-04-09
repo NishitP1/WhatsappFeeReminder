0a92412f32e356778bfbe2924b51a4cacaa0d88c
Nishit <94426361+NishitP1@users.noreply.github.com>
2025-04-10 00:34:20 +0530

    Update database.js
    fixed the env issue

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise');
-//hardcode the value if env is not working
+require('dotenv').config();
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
